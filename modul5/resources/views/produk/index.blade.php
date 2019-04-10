d6258f5c60f4a84390c5005871e7b71889fae078
dianra <dianramadani20121998@gmail.com>
2019-04-10 15:08:40 +0700

    tentang  controller

---
+++ modul5/resources/views/produk/index.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>Data Produk</h1>
+<table border="1">
+<thead>
+<tr>No</tr>
+<tr>Nama Produk</tr>
+</tr>
+</thead>
+<tbody>
+@foreach ($produk as $i => $v)
+<tr>
+<td>{{ $i+1 }}</td>
+<td>{{ $v }}</td>
+</tr>
+@endforeach
+</tbody>
+</table>
+</body>
+</html>
